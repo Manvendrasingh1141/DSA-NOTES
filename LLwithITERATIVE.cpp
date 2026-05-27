4bdde2ea39ffca2c7f6b71683ae9a651ac7077c0
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-05-27 11:25:58 +0530

    Implement iterative linked list creation and display

---
+++ LLwithITERATIVE.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+}; 
+
+Node *Create(int arr[],int n){
+    Node *start=NULL,*current=NULL;
+    
+    for(int i=0;i<n;i++){
+        if(start==NULL){
+            start = new Node(arr[i]);
+        }else{
+            Node *temp = new Node(arr[i]);
+            current->next = temp;
+            current=temp;
+        }
+    }
+    return start;
+    
+}
+
+void Display(Node *start){
+    Node *ptr = start;
+    while(ptr->next){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+
+int main(){
+
+    int arr[] = {1,2,3,4};
+    Node *start = Create(arr,4);
+    Display(start);
+}
