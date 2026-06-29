8547b843261d597d1d9bf03192690286efddf694
Manvendra Singh Shekhawat <152421486+Manvendrasingh1141@users.noreply.github.com>
2026-06-29 14:01:51 +0530

    Implement linked list reversal using recursion

---
+++ REVERSEaLLusingRECCURSION.cpp
@@ -0,0 +1,59 @@
+
+
+//REVERSE A LL BY RECCURSION
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+Node *Create(int arr[],int n){
+    Node *start=NULL,*current=NULL;
+    for(int i=0;i<n;i++){
+        if(start==NULL){
+            start = new Node(arr[i]);
+            current = start;
+        }else{
+        
+            Node *node = new Node(arr[i]);
+            current->next = node;
+            current = node;
+        }
+    }
+    return start;
+}
+
+
+Node* Reverse(Node *start , Node *prev){
+    Node *temp = NULL, *current=start;
+    if(current==NULL)return prev;
+    temp = current->next;
+    current->next = prev;
+    return Reverse(temp,current);
+    
+}
+
+void display(Node *root){
+    Node *ptr = root;
+    while(ptr){
+        cout<<ptr->data<<" ";
+        ptr = ptr->next;
+        
+    }    
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    Node *root = Create(arr,5);
+    Node *newNode = Reverse(root,NULL);
+    display(newNode);
+}
